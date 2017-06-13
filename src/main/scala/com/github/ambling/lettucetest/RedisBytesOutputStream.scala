45b2edef1f4e9f1210f094e7cc733ed0c7f6205b
ambling <ambling07@gmail.com>
2017-06-13 17:09:53 +0800

    use unbuffered channel

--- src/main/scala/com/github/ambling/lettucetest/RedisBytesOutputStream.scala
+++ src/main/scala/com/github/ambling/lettucetest/RedisBytesOutputStream.scala
@@ -2,6 +2,7 @@ package com.github.ambling.lettucetest
 
 import java.io.{Closeable, OutputStream}
 import java.nio.ByteBuffer
+import java.nio.channels.spi.AbstractInterruptibleChannel
 import java.nio.channels.{Channels, WritableByteChannel}
 
 import com.lambdaworks.redis.RedisFuture
@@ -31,19 +32,24 @@ class RedisBytesOutputStream(
     buf.putInt(b)
     buf.flip()
     buf.position(3)
-    val future = asyncCommand.append(key, buf)
-    futures += future
+    write(buf)
   }
 
   override def write(b: Array[Byte]): Unit = {
-    val future = asyncCommand.append(key, ByteBuffer.wrap(b))
-    futures += future
+    write(ByteBuffer.wrap(b))
   }
 
   override def write(b: Array[Byte], off: Int, len: Int): Unit = {
     val buf = ByteBuffer.wrap(b, off, len)
+    write(buf)
+  }
+
+  /**
+   * convenient write content in a bytebuffer
+   */
+  def write(buf: ByteBuffer): Unit = {
     val future = asyncCommand.append(key, buf)
-    futures += future
+    futures.append(future)
   }
 
   override def flush(): Unit = {
@@ -58,6 +64,44 @@ class RedisBytesOutputStream(
   }
 
   def getChannel: WritableByteChannel = {
-    Channels.newChannel(this)
+    new UnbufferedWritableByteChannel(this)
+  }
+}
+
+/**
+ * The WritableByteChannelImpl class defined in [[Channels]] is actually buffered and may lead to
+ * error if each write operation is not flushed immediately.
+ *
+ * The buffer is actually unnecessary since the input is already a buffer itself.
+ */
+class UnbufferedWritableByteChannel(val out: RedisBytesOutputStream)
+  extends AbstractInterruptibleChannel with WritableByteChannel {
+
+  private val TRANSFER_SIZE = 8192
+  private var open = true
+
+  override def write(src: ByteBuffer): Int = {
+    val len = src.remaining
+    var totalWritten = 0
+    this.synchronized {
+
+      while (totalWritten < len) {
+        val bytesToWrite = Math.min(len - totalWritten, TRANSFER_SIZE)
+        try {
+          begin()
+          val buf = src.duplicate()
+          buf.limit(buf.position() + bytesToWrite)
+          out.write(buf)
+          src.position(src.position() + bytesToWrite)
+        } finally end(bytesToWrite > 0)
+        totalWritten += bytesToWrite
+      }
+      return totalWritten
+    }
+  }
+
+  override protected def implCloseChannel(): Unit = {
+    out.close()
+    open = false
   }
 }
